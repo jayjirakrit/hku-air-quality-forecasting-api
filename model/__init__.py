87979a960872fd3d19b0ee65ce0399cb1be2ff3c
Jirakrit <jayjirakrit@gmail.com>
2025-07-16 15:19:15 +0800

    Remove recommendation

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,5 @@
 from .air_quality import AirQualityData
 from .station_model import StationModel
-from .recommendation import Recommendation
 
 # Optional: Shortcut to import all models at once
-__all__ = ["AirQualityData", "StationModel", "Recommendation"]
+__all__ = ["AirQualityData", "StationModel"]
